c35004a28b10cd8924ed447f704a3dd61a3a18d1
scoow <85620078+scoow@users.noreply.github.com>
2023-07-04 20:59:03 +0300

    Сделал префабы лайнов и тайлов

---
+++ Assets/Scripts/TileBacklight.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ZvP.UI
+{
+    public class TileBacklight : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
