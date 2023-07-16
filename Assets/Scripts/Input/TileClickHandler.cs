bb7a679b41253f1bfef5fce497565778dc0265c1
scoow <85620078+scoow@users.noreply.github.com>
2023-07-16 21:05:44 +0300

    Спавн растения по нажатию

--- Assets/Scripts/Input/TileClickHandler.cs
+++ Assets/Scripts/Input/TileClickHandler.cs
@@ -4,6 +4,8 @@ using UnityEngine.EventSystems;
 public class TileClickHandler : MonoBehaviour, IPointerClickHandler
 {
     private UIManager _uiManager;
+    [SerializeField] private GameObject _simplePlantPrefab;
+    [SerializeField] private GameObject _meleePlantPrefab;
 
     private void Start()
     {
@@ -15,5 +17,17 @@ public class TileClickHandler : MonoBehaviour, IPointerClickHandler
 
         _uiManager._inputStateMachine.SetNewState(InputStateType.defaultState);
         Debug.Log($"Установлено растение {_uiManager._selectedPlantType}");
+
+        switch (_uiManager._selectedPlantType)
+        {
+            case PlantType.simplePlant:
+                Instantiate( _simplePlantPrefab, transform.position, Quaternion.identity );
+                break;
+            case PlantType.meleePlant:
+                Instantiate(_meleePlantPrefab, transform.position, Quaternion.identity);
+                break;
+            default:
+                break;
+        }
     }
 }
